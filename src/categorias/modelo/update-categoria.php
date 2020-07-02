a804418396365d0269b4492d2cf90da071b61336
rafaelcruz72 <rafaelcruz72@hotmail.com>
2020-07-02 12:02:35 -0300

    Commit dos WS update e delete categoria

--- src/categorias/modelo/update-categoria.php
+++ src/categorias/modelo/update-categoria.php
@@ -1,52 +1,47 @@
 <?php
 
-include('../../banco/conexao.php');
+    include('../../banco/conexao.php');
 
-if(!$conexao){
-    $dados = array(
-        'tipo' => 'info',
-        'mensagem' => 'OPS, não foi possível obter uma conexão com o banco de dados, tente mais tarde..'
-    );
-} else{
+    if($conexao){
 
-    $requestData = $_REQUEST;
+        $requestData = $_REQUEST;
 
-    if(empty($requestData['nome']) || empty($requestData['ativo']) ){
-        $dados = array(
-            'tipo' => 'info',
-            'mensagem' => 'Existe(m) campo(s) obrigatório(s) vazio(s).'
-        );
+        if(empty($requestData['nome']) || empty($requestData['ativo'])){
+            $dados = array(
+                "tipo" => "info",
+                "mensagem" => "Existe(m) campo(s) obrigatório(s) em branco."
+            );
+        } else{
+            $id = isset($requestData['idcategoria']) ? $requestData['idcategoria'] : '';
+            $requestData['ativo'] = $requestData['ativo'] == "on" ? "S" : "N";
+
+            $date = date_create_from_format('d/m/Y H:i:s', $requestData['dataagora']);
+            $requestData['dataagora'] = date_format($date, 'Y-m-d H:i:s');
+
+            $sql = "UPDATE categorias SET nome = '$requestData[nome]', ativo = '$requestData[ativo]', datamodificacao = '$requestData[dataagora]' WHERE idcategoria = $id ";
+
+            $resultado = mysqli_query($conexao, $sql);
+
+            if($resultado){
+                $dados = array(
+                    "tipo" => "success",
+                    "mensagem" => "Categoria alterada com sucesso."
+                );
+            } else {
+                $dados = array(
+                    "tipo" => "error",
+                    "mensagem" => mysqli_error($conexao) //"Não foi possível altarar a categoria."
+                );
+            }
+        }
+
+        mysqli_close($conexao);
+
     } else {
-
-        //$requestData = array_map('utf8_decode', $requestData);
-
-        $id = isset($requestData['idcategoria']) ? $requestData['idcategoria'] : '';
-
-        $requestData['ativo'] = $requestData['ativo'] == "on" ? "S" : "N";
-
-        //$requestData['dataagora'] = date('Y-m-d H:i:s', strtotime($requestData['dataagora']));
-
-        $date = date_create_from_format('d/m/Y H:i:s', $requestData['dataagora']);
-        $requestData['dataagora'] = date_format($date, 'Y-m-d H:i:s');
-
-        $sqlComando = "UPDATE categorias SET nome = '$requestData[nome]', ativo = '$requestData[ativo]', datamodificacao = '$requestData[dataagora]'  WHERE idcategoria = $id ";
-
-        $resultado = mysqli_query($conexao, $sqlComando);
-
-         if($resultado){
-            $dados = array(
-                'tipo' => 'success',
-                'mensagem' => 'Categoria alterada com sucesso.'
-            );
-         } else{
-            $dados = array(
-                'tipo' => 'error',
-                'mensagem' => 'Não foi possível alterar a categoria.'.mysqli_error($conexao)
-            );
-         }
+        $dados = array(
+            "tipo" => "info",
+            "mensagem" => "Ops... não foi possível conectar ao banco de dados"
+        );
     }
 
-    mysqli_close($conexao);
-}
-
-echo json_encode($dados, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+   echo json_encode($dados, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE); 
